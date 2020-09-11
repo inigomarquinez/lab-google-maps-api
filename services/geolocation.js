5eb6b0cf50263948632662b9583b86dce061cf17
Íñigo Marquínez <inigo.marquinez@guidesmiths.com>
2020-09-11 19:31:57 +0200

    feat: added google maps api library

--- services/geolocation.js
+++ services/geolocation.js
@@ -1,5 +1,22 @@
+const { Client } = require('@googlemaps/google-maps-services-js');
+
 const geolocation = () => {
-  console.log('run geolocation');
+  const client = new Client({});
+
+  client
+    .elevation({
+      params: {
+        locations: [{ lat: 45, lng: -110 }],
+        key: process.env.GOOGLE_MAPS_API_KEY,
+      },
+      timeout: 1000, // milliseconds
+    })
+    .then((r) => {
+      console.log(r.data.results[0].elevation);
+    })
+    .catch((e) => {
+      console.log(e.response.data.error_message);
+    });
 };
 
 module.exports = geolocation;
